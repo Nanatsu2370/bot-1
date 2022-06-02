59c8d5f831f4a369536e636595ae8e0138b9dedc
elier008 <106573917+elier008@users.noreply.github.com>
2022-06-02 01:50:57 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,6 @@
+import os
+
+api_id = '10318505'
+api_hash = '15e1c2c351ef0855e36d44facec126b6'
+bot_token = '5498148937:AAEN56m6TxRJuaFiJB-hDC-B8QnTbosLhiA'
+bot_admin_group = os.environ.get('bot_admin_group')
