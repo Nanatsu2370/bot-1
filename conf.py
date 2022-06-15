40d39ae1c714646f894c3b19f8abb68b24c0b8c5
Nanatsu2370 <94955661+Nanatsu2370@users.noreply.github.com>
2022-06-15 13:15:48 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,5 +2,5 @@ import os
 
 api_id = '10318505'
 api_hash = '15e1c2c351ef0855e36d44facec126b6'
-bot_token = '5498148937:AAEN56m6TxRJuaFiJB-hDC-B8QnTbosLhiA'
-bot_admin_group = os.environ.get('bot_admin_group')
+bot_token = '5233279296:AAH1UHHO4xGJRUeANARpuUrLTzQ3jxBP6b8'
+
